d85f032108839f6e3a1d89d665f0dc325bb12399
naosz <aronfabian77@gmail.com>
2017-08-20 22:07:45 +0200

    creating the sum values for facesums. added fun.R as function storage.

--- Extracting_data.R
+++ Extracting_data.R
@@ -1,2 +1,10 @@
-myString <- "Hello, World!"
-print ( myString)
+library(tcltk)
+library(dplyr)
+
+source("fun.R")
+load_date("TESLA","SYB1S")
+
+facesum <- result1[2]
+positivesum <- colSums(facesum*(facesum > 0))
+negativesum <- colSums(facesum*(facesum < 0))
+allsum <- colSums(result1[2])
