a7de7b1abb2aa61253a198e09019961eb7a179f2
Aron Fabian <aronfabian77@gmail.com>
2017-08-22 21:10:59 +0000

    adding comment for tomorrow not to forget

--- Extracting_data.R
+++ Extracting_data.R
@@ -8,3 +8,7 @@ facesum <- result1[2]
 positivesum <- colSums(facesum*(facesum > 0))
 negativesum <- colSums(facesum*(facesum < 0))
 allsum <- colSums(result1[2])
+
+#need to make DF <- month/average/list of funds
+
+print(allsum)
